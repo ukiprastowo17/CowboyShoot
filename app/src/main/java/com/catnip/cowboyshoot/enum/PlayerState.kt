26dc6a74d7e77255f56482c55312c8e1b5c33aa7
Muhammad Hermas Yuda P <hermas.yuda@gmail.com>
2022-09-19 21:57:20 +0700

    Init project and preparing resource

---
+++ app/src/main/java/com/catnip/cowboyshoot/enum/PlayerState.kt
@@ -0,0 +1,11 @@
+package com.catnip.cowboyshoot.enum
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+enum class PlayerState {
+    IDLE,
+    SHOOT,
+    DEAD
+}
---
+++ app/src/main/java/com/catnip/cowboyshoot/model/Player.kt
@@ -0,0 +1,16 @@
+package com.catnip.cowboyshoot.model
+
+import com.catnip.cowboyshoot.enum.PlayerPosition
+import com.catnip.cowboyshoot.enum.PlayerSide
+import com.catnip.cowboyshoot.enum.PlayerState
+
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+data class Player(
+    val playerSide: PlayerSide,
+    val playerState: PlayerState,
+    val playerPosition: PlayerPosition
+)
